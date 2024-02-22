0254c5cf198dc1eb7b428a25ed12c4363e29074a
Mahdiyar <m.poursalari3200@gmail.com>
2024-02-22 16:55:54 +0330

    shazamCore services were added.

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import { shazamCoreApi } from "../services/shazamCore";
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    [shazamCoreApi.reducerPath]: shazamCoreApi.reducer
+  },
+  middleware: (getDefaultMiddleWare) => getDefaultMiddleWare().concat(shazamCoreApi.middleware)
 });
 
 export default store;
