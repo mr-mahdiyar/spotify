0cea34e7d015cfb9fe414bf3968753540d528191
Mahdiyar <m.poursalari3200@gmail.com>
2024-02-22 15:47:33 +0330

    store was added.

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import { routes } from './Routes'
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={routes  }/>
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { routes } from "./Routes";
+import store from "./app/store";
+import { Provider } from "react-redux";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <RouterProvider router={routes} />
+  </Provider>
+);
