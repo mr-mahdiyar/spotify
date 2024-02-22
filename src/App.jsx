342689df5ffaf91e49d28a0b8ecb8009269d6123
Mahdiyar <m.poursalari3200@gmail.com>
2024-02-22 14:34:23 +0330

    In the name of god, Starting point.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h2 className='bg-red-600 text-2xl p-4 text-white text-center'>In the name of god</h2>
+    </div>
+  )
+}
+
+export default App
