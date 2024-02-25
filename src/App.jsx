b8beef20a97c6545b243778d5f7cc911e37984ad
mr-mahdiyar <m.poursalari3200@gmail.com>
2024-02-25 23:59:45 +0330

    TopPlay was added to index and app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
-import  MusicPlayer from "./components/MusicPlayer/"
+import { MusicPlayer, Sidebar, TopPlay } from "./";
 const App = () => {
-  const { activeSong } = useSelector((state) => state.player)
+  const { activeSong } = useSelector((state) => state.player);
   return (
     <div className="relative flex">
+      <Sidebar />
       <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
         <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
           <div className="flex-1 h-fit pb-40">
             <Outlet />
+          </div>
+          <div className="xl:sticky relative top-0 h-fit">
+            <TopPlay />
           </div>
         </div>
       </div>
--- src/index.jsx
+++ src/index.jsx
@@ -4,5 +4,6 @@ export { default as SongCart } from "./components/SongCart";
 export { default as Loader } from "./components/Loader";
 export { default as Error } from "./components/Error";
 export { default as PlayPause } from "./components/PlayPause";
-export { default as Sidebar } from "./components/Sidebar"
-export { default as MusicPlayer } from "./components/MusicPlayer/"
+export { default as Sidebar } from "./components/Sidebar";
+export { default as MusicPlayer } from "./components/MusicPlayer/";
+export { default as TopPlay } from "./components/TopPlay"
