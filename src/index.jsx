abf9c17daf35a015400221b31a1f9af193f4b922
mr-mahdiyar <m.poursalari3200@gmail.com>
2024-02-25 12:04:50 +0330

    MusicPlayer and SideBar were added.

--- src/index.jsx
+++ src/index.jsx
@@ -4,3 +4,5 @@ export { default as SongCart } from "./components/SongCart";
 export { default as Loader } from "./components/Loader";
 export { default as Error } from "./components/Error";
 export { default as PlayPause } from "./components/PlayPause";
+export { default as Sidebar } from "./components/Sidebar"
+export { default as MusicPlayer } from "./components/MusicPlayer/"
