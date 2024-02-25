9efed5cc370791835ef3915482564e16d76b6f46
mr-mahdiyar <m.poursalari3200@gmail.com>
2024-02-25 07:16:14 +0330

    some styles were added to app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { Outlet } from "react-router-dom";
 const App = () => {
   return (
-    <div>
-      <Outlet />
+    <div className="relative flex">
+      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
+        <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
+          <div className="flex-1 h-fit pb-40"></div>
+          <Outlet />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
